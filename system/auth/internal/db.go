46a78edb8ae3c87dd1e37480c8083505c3034374
Lorenzo <l.bartolini02@gmail.com>
2025-01-19 10:52:09 +0100

    Documentation for Auth

--- system/auth/internal/db.go
+++ system/auth/internal/db.go
@@ -5,11 +5,13 @@ import (
 	"log"
 )
 
+// Interface with the Auth Database
 type AuthDB interface {
 	Login(username string, password string) (bool, error)
 	Register(username string, password string, email string, phone string) (bool, error)
 }
 
+// Implementation for an SQL Database
 type SQL_DB struct {
 	db *sql.DB
 }
@@ -19,6 +21,9 @@ func NewSQL_DB(conn *sql.DB) *SQL_DB {
 }
 
 func (s *SQL_DB) Login(username string, password string) (bool, error) {
+	// Perform Login inside the DB
+	// if credentials are not correct no row will be retrieved resulting in an erroneous login action
+
 	stmt, err := s.db.Prepare("SELECT Username FROM Users WHERE Username=? AND Password=?")
 	if err != nil {
 		log.Println(err)
@@ -36,6 +41,9 @@ func (s *SQL_DB) Login(username string, password string) (bool, error) {
 }
 
 func (s *SQL_DB) Register(username string, password string, email string, phone string) (bool, error) {
+	// Perform Registration inside the DB
+	// if username is already present the registration fails, the error is picked when executing the statement and also by checking the affected rows
+
 	stmt, err := s.db.Prepare("INSERT INTO Users VALUES (?, ?, ?, ?)")
 	if err != nil {
 		log.Println(err)
--- system/auth/internal/server.go
+++ system/auth/internal/server.go
@@ -20,16 +20,18 @@ func NewServer(conn AuthDB) *Server {
 }
 
 func (s *Server) Login(ctx context.Context, in *pb.PlayerCredentials) (*pb.AuthResult, error) {
-	res, err := s.db.Login(in.Username, in.Password)
+	// Procedure to perform Login in the system
 
+	res, err := s.db.Login(in.Username, in.Password)
 	log.Printf("Login (%s:%s) with result %t", in.Username, in.Password, res)
 
 	return &pb.AuthResult{Result: res}, err
 }
 
 func (s *Server) Register(ctx context.Context, in *pb.PlayerDetails) (*pb.AuthResult, error) {
-	res, err := s.db.Register(in.Username, in.Password, in.Email, in.Phone)
+	// Procedure to perform Registration in the system
 
+	res, err := s.db.Register(in.Username, in.Password, in.Email, in.Phone)
 	log.Printf("Register (%s:%s:%s:%s) with result %t", in.Username, in.Password, in.Email, in.Phone, res)
 
 	return &pb.AuthResult{Result: res}, err
